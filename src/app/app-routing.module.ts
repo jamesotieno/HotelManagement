fde7fd0cc375dc0d4a9d8ad15cb113d79874fd3b
James Otieno <aggrey.otieno@gmail.com>
2022-01-24 19:13:58 +0300

    updated frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { RoomBooKingRequestComponent } from './room-boo-king-request/room-boo-king-request.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'auth', component: AuthenticationComponent },
+  { path: 'room-booking-request', component: RoomBooKingRequestComponent },
+  { path: 'feedback', component: FeedbackComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
